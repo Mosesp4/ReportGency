ca507814630109cdd4ba4b3d0296cf5a53642663
Mosesp4 <echelachris@gmail.com>
2025-03-02 19:53:48 +0100

    resolve crypto module

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
       serverActions: true, // Ensures that dynamic server actions work correctly
     },
     output: "standalone", // Ensures compatibility with dynamic API routes
+
+    webpack: (config) => {
+        config.resolve.fallback = {
+          crypto: require.resolve("crypto-browserify"),
+        };
+        return config;
+      },
   };
   
   export default nextConfig;
